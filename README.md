ae06cd243a14e2e06542f78a09f8d34297d55d66
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2022-07-17 21:54:38 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Hello, World!
+
+## JS Edition
+
+The intro.js file contains examples of JavaScript **expressions** that you can output to the console using `console.log`.
+
+### Instructions
+
+1. Open `index.html` in the browser (you can do this at the command line: run `open index.html` in this project directory).
+2. In the browser where your page is open, open the dev tools, and click on the console tab.
+3. In VS Code, open `intro.js` and work on the exercises in there.
+   - Read the comments for further instructions.
+   - Make sure to go in order. Later lines of code won't work if you haven't followed the instructions that came before.
+   - Look for the smiley cat 😺 for the tasks you need to do.
+   - Uncomment or replace lines that start with `//` as you work your way through each exercise.
+   - Make sure to save the file and reload the browser page to run the code each time you make a change and want to see what happens.
+
+### How comments in JavaScript work
+
+_Comments_ in code are meant for humans to read, not for the computer. To me more specific, it's the JavaScript interpreter that runs each line of JavaScript code.
+
+`//` at the start of the line means the interpreter will not run that line
+`/*` at the start of a line begins a multiline comment
+`*/` ends a multiline comment
+
+Whatever is included between `/*` and `*/` will be ignored by the interpreter.
+
+You can add or remove the characters for comments in order to run or ignore specific lines of code.
+
+When comment characters are added to a line of code, we say that the line is "commented out" -- that means it will not run.
---
+++ intro.js
@@ -0,0 +1,96 @@
+// Intro JS exercise
+console.log('oh hai! 👋 Welcome to the console!')
+console.log(
+  'This is where you can see output that you have printed out (_logged_) from your JavaScript files using console.log().'
+)
+console.log(
+  'You can also run JavaScript directly here, just start typing. When you press return or enter, the JavaScript you typed will be evaluated.'
+)
+
+// Read carefully and uncomment the necessary lines one at a time as you run the code.
+
+/*
+ASSIGN A VALUE TO A VARIABLE AND LOG IT TO THE CONSOLE
+
+Read the code below.
+*/
+let firstName = 'Jamal'
+console.log("Here's some output from our first line of JavaScript in intro.js")
+console.log(
+  "On the next line 👇 we'll log the value of the firstName variable."
+)
+console.log(firstName)
+
+/*
+YOUR TURN.
+😺 Write a value for the variable defined on line 28. It's commented out so that it won't cause an error.
+Once you have written the value, remove the comment characters, save this file, and reload the page in the browser.
+/*
+// const myName =
+// console.log(myName)
+
+
+/*
+ASSIGN A NEW VALUE TO A PREVIOUSLY DEFINED VARIABLE
+
+Below, a new value is assigned to the firstName variable that we defined on line 17 above.
+Read the code. What do you expect that the `firstName` variable will evaluate to after reassignment?
+
+😺 Uncomment lines 41 and 42, save this file, reload the browser page, and check the output in the console:
+*/
+// firstName = "Avery"
+// console.log("The firstName variable now has an assigned value of ", firstName)
+
+/*
+CREATE A NEW VARIABLE USING THE VALUE FROM ANOTHER VARIABLE
+
+What is happening here? Use console.log() to check if you're right.
+
+😺 Uncomment the following lines:
+*/
+// let name = firstName
+// console.log("The name variable evaluates to ", name)
+
+/*
+😺 Create a variable named `greeting`.
+Assign it a value of a string like "Hi" or "Hey there" (or anything you like!).
+Log the `greeting` variable's value to the console.
+*/
+// Put your code here!
+
+/* COMBINE STRINGS USING VARIABLES
+
+😺 Uncomment line 65 to see how you can combine strings with variables to create a dynamic sentence!
+/*
+console.log("Nice to meet you, " + firstName + "! My name is " + myName + ".")
+
+😺 Combine the `greeting` and `name` variables to output a personalized greeting like "Hi, Avery!".
+Log it to the console.
+*/
+// Write your code here
+
+/*
+USING VARIABLES WITH MATH
+
+What do you expect that the variable `result` will evaluate to?
+😺 Uncomment the following lines and use console.log() to check what value is stored in the variable `result`
+*/
+// let num1 = 50
+// let num2  = 10
+// let result = num1 - num2
+
+/* What will `result` return here? Read this code carefully.
+😺 Uncomment line 85, and write a console.log to see what `result` will evaulate to.
+*/
+// result = num1 - "5"
+// replace this line with a console.log() that shows what `result` is.
+
+/*
+😺 Create a variable to calculate the total number of tacos and burritos.
+Log to the console a sentence that tells the user the total number, for instance:
+"The total number of tacos and burritos is 12"
+Uncomment the following lines and then write your code.
+*/
+// let tacos = 8
+// let burritos = 4
+// Write your code here!
